8de347c9547217d2f67fdcc9f0ec5ec7fb8a8b65
FherEnrique <enquique500@gmail.com>
2021-02-14 17:27:16 -0600

    Base2.0

--- index.php
+++ index.php
@@ -7,40 +7,45 @@
     <script src="./js/jquery.js"></script>
     <title>Beca "N de Nayib"</title>
     <script src="https://kit.fontawesome.com/2944ad1077.js" crossorigin="anonymous"></script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <!-- <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script> -->
+    <style>
+        #error{
+            color: red;
+        }
+    </style>
 </head>
 <body>
 <section class="lo">
     <form method="POST" role="form"><br>
-        <label for="inp1">Nombres del interesado:</label>
+        <label >Nombres del interesado:</label>
         <div class="input-group margin-bottom-sm">
             <input class="form-control" type="text" placeholder="Ingrese parametros de busqueda" name="nombres" id="nombres" autocomplete="off" required onkeyup="funcnombres()"><br>
-            <div id="resultadoBusqueda" class="container"></div>
+            <div id="resultadonombres" class="container"><i class="fas fa-exclamation-circle" id="error"></i> Verificacion ha fallado</div>
         </div><br><br>
-        <label for="inp2">Apellidos del interesado</label>
+        <label>Apellidos del interesado</label>
         <div class="input-group margin-bottom-sm">
             <input class="form-control" type="text" placeholder="Ingrese parametros de busqueda" name="busqueda" id="busqueda" autocomplete="off" required onkeyup="buscar();"><br>
-            <div id="resultadoBusqueda" class="container"></div>
+            <div id="resultadoBusqueda" class="container"><i class="fas fa-exclamation-circle" id="error"></i> Verificacion ha fallado</div>
         </div><br><br>
-        <label for="inp1">Celular/Telefono del interesados</label>
+        <label>Número de contacto del interesados</label>
         <div class="input-group margin-bottom-sm">
             <input class="form-control" type="text" placeholder="Ingrese parametros de busqueda" name="busqueda" id="busqueda" autocomplete="off" required onkeyup="buscar();"><br>
-            <div id="resultadoBusqueda" class="container"></div>
+            <div id="resultadoBusqueda" class="container"><i class="fas fa-exclamation-circle" id="error"></i> Verificacion ha fallado</div>
         </div><br><br>
-        <label for="inp1">Parametros de busquedad (modelo, empresa)</label>
+        <label>Correo del interesado</label>
         <div class="input-group margin-bottom-sm">
             <input class="form-control" type="text" placeholder="Ingrese parametros de busqueda" name="busqueda" id="busqueda" autocomplete="off" required onkeyup="buscar();"><br>
-            <div id="resultadoBusqueda" class="container"></div>
+            <div id="resultadoBusqueda" class="container"><i class="fas fa-exclamation-circle" id="error"></i> Verificacion ha fallado</div>
         </div><br><br>
-        <label for="inp1">Parametros de busquedad (modelo, empresa)</label>
+        <label>Ingresos promedios mensuales</label>
         <div class="input-group margin-bottom-sm">
             <input class="form-control" type="text" placeholder="Ingrese parametros de busqueda" name="busqueda" id="busqueda" autocomplete="off" required onkeyup="buscar();"><br>
-            <div id="resultadoBusqueda" class="container"></div>
+            <div id="resultadoBusqueda" class="container"><i class="fas fa-exclamation-circle" id="error"></i> Verificacion ha fallado</div>
         </div><br><br>
-        <label for="inp1">Parametros de busquedad (modelo, empresa)</label>
+        <labe>Número de habitantes en la casa</label>
         <div class="input-group margin-bottom-sm">
             <input class="form-control" type="text" placeholder="Ingrese parametros de busqueda" name="busqueda" id="busqueda" autocomplete="off" required onkeyup="buscar();"><br>
-            <div id="resultadoBusqueda" class="container"></div>
+            <div id="resultadoBusqueda" class="container"><i class="fas fa-exclamation-circle" id="error"></i> Verificacion ha fallado</div>
         </div><br><br>
     </form>
 </section>
@@ -48,18 +53,16 @@
 
 
 <script>
-$(document).ready(function() {$("#resultadoBusqueda").html('Verificacion ha fallado');
-});
 
 function funcnombres() {
     var textoBusqueda = $("input#nombres").val();
  
      if (textoBusqueda != "") {
         $.post("./php/func1.php", {valorBusqueda: textoBusqueda}, function(mensaje) {
-            $("#resultadoBusqueda").html(mensaje);
+            $("#resultadonombres").html(mensaje);
          }); 
      } else { 
-        $("#resultadoBusqueda").html('Verificacion ha fallado');
+        $("#resultadonombres").html('Verificacion ha fallado');
      };
 };
 </script>
